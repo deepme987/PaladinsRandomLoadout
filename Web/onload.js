89807e1c2982c064452a58b8b6e5720b0f6932af
deepme987 <deepme987@gmail.com>
2019-01-03 18:25:01 +0530

    Added loading logo + transitions

--- Web/onload.js
+++ Web/onload.js
@@ -1,12 +1,13 @@
 document.onreadystatechange = function () {
 	var state = document.readyState
 	  if (state == 'interactive') {
-	       document.getElementById('contents').style.visibility="hidden";
+	       $("#contents").hide();
 	  } else if (state == 'complete') {
 	      setTimeout(function(){
 	         document.getElementById('interactive');
-	         document.getElementById('load').style.visibility="hidden";
-	         document.getElementById('contents').style.visibility="visible";
+	         $("#load").hide(100);
+	         // document.getElementById('contents').style.visibility="visible";
+	         $("#contents").fadeIn(700);
 	      },1000);
 	  }
 }
